e99d3bc8751918222c440c96daea32db19ee4b6f
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-12-30 20:16:05 +0800

    Update element.hpp

--- src/svg/element.hpp
+++ src/svg/element.hpp
@@ -11,28 +11,32 @@ namespace svg {
 
 class element {
 public:
-  typedef std::unique_ptr<element> element_ptr;
+  typedef std::unique_ptr<element> object_type;
 
 public:
   virtual int indent() const = 0;
   virtual bool is_self_closing() const = 0;
   virtual std::string tag() const = 0;
   virtual std::string attributes() const = 0;
-  virtual std::vector<element_ptr> &children() = 0;
-  virtual std::vector<const element_ptr> const &children() const = 0;
+  virtual std::string &text() = 0;
+  virtual std::string const &text() const = 0;
+  virtual std::vector<object_type> &children() = 0;
+  virtual std::vector<object_type> const &children() const = 0;
 
   operator std::string() const {
     return to_string();
   }
 
   std::string to_string() const {
-    return _to_string(0);
+    return _to_string(-indent());
   }
 
 private:
-  std::string _to_string(int indentation) const {
+  std::string _to_string(int base_indent) const {
+    base_indent += indent();
+
     std::string result(
-      std::string(indentation, ' ') + 
+      std::string(base_indent, ' ') + 
       "<" + tag() + " " + attributes()
     );
 
@@ -40,12 +44,16 @@ private:
       return result + "/>\n";
     }
 
-    result += ">\n";
+    result += 
+      ">\n" + 
+      std::string(base_indent, ' ') + 
+      text() + "\n";
+    
     for (auto &e: children()) {
-      result += e->_to_string(indentation + e->indent());
+      result += e->_to_string(base_indent);
     }
     result +=
-      std::string(indentation, ' ') + 
+      std::string(base_indent, ' ') + 
       "</" + tag() + ">\n";
 
     return result;
