152538b8ca36784469d87eea008bfb0f7faedc7f
AsherChen <gpks556@gmail.com>
2023-01-06 17:56:20 +0800

    Asher Chen commited at 2023-01-06_17-56-20

--- debug/graph/pair_test_with_cxxgraph.cpp
+++ debug/graph/pair_test_with_cxxgraph.cpp
@@ -1,4 +1,5 @@
 #include "../../src/graph/graph.hpp"
+#include "../../third-party/CXXGraph/include/CXXGraph.hpp"
 #include <iostream>
 #include <fstream>
 #include <cassert>
@@ -25,9 +26,11 @@ static std::mt19937 rng(std::random_device{}());
 
 // 1. Generate a cycle of 1000 nodes. Each edge has length 1.
 // 2. Add x random edges. Each random edge has the same length y. 
-
+//
 int main() {
-  gh::index_type  x(uidist(1, 1000 * 2)(rng));
-  gh::length_type y(uidist(1, 1000 / 2)(rng));
-  
+  gh::index_type  n(1000);
+  gh::index_type  x(uidist(1, n * 2)(rng));
+  gh::length_type y(uidist(1, n / 2)(rng));
+
+  // for ()
 }
